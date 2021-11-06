6bdfa3fc51711cc0598dd128e065f86ba0352957
Sagun Shrestha <goodsamaritan.4846@gmail.com>
2021-11-05 17:41:36 -0700

    Display all the new comment and rating in the index page using a nested for loop to access the review and ratings

--- controllers/trails.js
+++ controllers/trails.js
@@ -2,12 +2,18 @@ import {Trail} from '../models/trail.js'
 
 
 function index(req,res){
-  Trail.find({},function(err,trail){
-    res.render('trails/index',{trail,title:'All Trails',user:req.user})
-    console.log(trail)
-  })
+  Trail.find({})
+  // .populate('content')
+  .then((trail)=>{
 
-}
+    res.render('trails/index',{trail,title:'All Trails',user:req.user})
+    console.log(trail[0].content[0].review)
+  }
+  )
+    .catch((err)=> console.log(err))
+  }
+
+
 
 function newTrail(req,res){
   res.render('trails/new',{title:'Add a trail',user:req.user})
@@ -17,10 +23,11 @@ function createTrail(req,res){
   //collect form data and put in inside Trail->content
 
   const trail = new Trail()
-  trail.content=req.body.comments
+  trail.content=req.body
   
-  console.log(trail.content)
+  // console.log(trail.content) 
   trail.save()
+
   .then(()=>res.redirect('/trails'))
 
   // console.log(req.body)
--- models/trail.js
+++ models/trail.js
@@ -1,10 +1,15 @@
 import mongoose from 'mongoose'
 
+const contentSchema = new mongoose.Schema({
+  review:{type:String,required:true},
+  rating:{type:Number,required:true}
+})
+
 const trailSchema = new mongoose.Schema({
   name: String,
-  content: String,
+  content: [contentSchema],
   place:String,
-  rating:Number
+  
 }, {
   timestamps: true
 })
